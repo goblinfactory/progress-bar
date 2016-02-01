157323e55e514b435104036d6c59815e8e459ead
Alan Hemmings <goblinfactory@gmail.com>
2016-02-01 02:10:30 +0000

    removed unused 'using'

--- Goblinfactory.ProgressBar.Tests/ProgressBarAcceptanceTests.cs
+++ Goblinfactory.ProgressBar.Tests/ProgressBarAcceptanceTests.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using ApprovalTests;
 using ApprovalTests.Namers;
